02c6f4f79282f02e51f3badbac8302b2bd2da098
lenovo <upasinduprabashwara@gmail.com>
2025-10-28 19:18:19 +0530

    latest

--- src/main/java/lk/ijse/cmjd/researchtracker/project/controller/ProjectController.java
+++ src/main/java/lk/ijse/cmjd/researchtracker/project/controller/ProjectController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
 @RestController
-@RequestMapping("/api/projects/")
+@RequestMapping("/api/project/")
 @RequiredArgsConstructor
 public class ProjectController {
 
